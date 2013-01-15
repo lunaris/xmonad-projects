442e48d3a3987a406c3147976f92ad70e4975266
Will Jones <will@sacharissa.co.uk>
2013-01-15 08:47:14 +0000

    Build up some useful bindings for projects.

--- xmonad.hs
+++ xmonad.hs
@@ -8,26 +8,23 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Prompt
 import XMonad.Prompt.Workspace
 import XMonad.Util.EZConfig
+import XMonad.Util.Run (safeSpawn, spawnPipe)
 
 import qualified XMonad.StackSet as SS
 
-xftFont
+_xftFont
   = "Monaco:pixelsize=14:antialias=true"
 
 _staticWorkspaces
-  = [ "dashboard"
-    , "irssi"
-    , "audio"
-    , "web"
-    ]
+  = ["6", "7", "8", "9", "0"]
 
 _projectsConfig
   = ProjectsConfig
-      { _pcDefaultWorkspaceId     = "dashboard"
-      , _pcWorkspaceIdsPerProject = ["local", "remote", "gimp"]
+      { _pcDefaultWorkspaceId     = "0"
+      , _pcWorkspaceIdsPerProject = ["1", "2", "3", "4", "5"]
       }
 
-dzenPP handle
+_dzenPP handle
   = defaultPP { _ppCurrent          = dzenColor "#fdddef" "#353535" .
                                         pad
 
@@ -41,7 +38,7 @@ dzenPP handle
               , _ppUrgent           = id
               , _ppWsSep            = ""
               , _ppSep              = ""
-              , _ppProject          = id
+              , _ppProject          = pad
 
               , _ppLayout           = dzenColor "#fdddef" "#353535" . pad
 
@@ -50,29 +47,53 @@ dzenPP handle
               , _ppOutput           = hPutStrLn handle
               }
 
-leftDzen
+_leftDzen
   = defaultDzenConfig { dzcWidth      = Just (Percent 50)
-                      , dzcFont       = Just xftFont
+                      , dzcFont       = Just _xftFont
                       , dzcForeground = Just "#fdddef"
                       , dzcBackground = Just "#222222"
                       }
 
+_rightDzen
+  = defaultDzenConfig { dzcX          = Just (Percent 50)
+                      , dzcWidth      = Just (Percent 50)
+                      , dzcAlignment  = Just AlignRight
+                      , dzcFont       = Just _xftFont
+                      , dzcForeground = Just "#fdddef"
+                      , dzcBackground = Just "#222222"
+                      }
+
+_dmenuArguments
+  = [ "-nb", "#222222"
+    , "-nf", "#fdddef"
+    , "-sb", "#353535"
+    , "-sf", "#fdddef"
+    , "-fn", _xftFont
+    ]
+
 _keys
-  = [ ("M-g", workspacePrompt defaultXPConfig (windows . SS.greedyView))
-    , ("M-a p", addProject "pp")
-    , ("M-a c", addProject "crs")
-    , ("M-r p", removeProject "pp")
-    , ("M-r c", removeProject "crs")
-    , ("M-s p", selectProject "pp")
-    , ("M-s c", selectProject "crs")
+  = [ ("M-a a", addProject "")
+    , ("M-a s", selectProject "")
+    , ("M-a d", removeProject "")
+
+    , ("M-b", sendMessage ToggleStruts)
+    , ("M-p", safeSpawn "dmenu_run" _dmenuArguments)
+    , ("M-s", safeSpawn "/home/will/.dzen/conch" (_terminal : _dmenuArguments))
+
+    , ("M4-f", spawn $
+        "google-chrome --disk-cache-dir=\"/tmp/google-chrome-cache\" " ++
+          "--disk-cache-size=" ++ show (128 * 1024 * 1024))
+
+    , ("M4-t", spawn _terminal)
+    , ("M4-x", spawn "slock")
     ] ++
 
-    [ ("M-" ++ show i, withCurrentProjectNthWorkspace SS.greedyView (i - 1))
-      | i <- [1..3]
+    [ ("M-" ++ m ++ show i, withCurrentProjectNthWorkspace f (i - 1))
+      | i <- [1..5], (m, f) <- [("", SS.greedyView), ("S-", SS.shift)]
       ] ++
 
-    [ ("M-S-" ++ show i, withCurrentProjectNthWorkspace SS.shift (i - 1))
-      | i <- [1..3]
+    [ ("M-" ++ m ++ show i, windows (f i))
+      | i <- _staticWorkspaces, (m, f) <- [("", SS.greedyView), ("S-", SS.shift)]
       ]
 
 _terminal
@@ -81,12 +102,13 @@ _terminal
 main :: IO ()
 main
   = do
-      leftHandle <- spawnDzen leftDzen
+      lh <- spawnDzen _leftDzen
+      spawnToDzen "conky -c ~/.dzen/conky" _rightDzen
 
       xmonad $ defaultConfig
         { borderWidth = 0
         , layoutHook  = avoidStruts (layoutHook defaultConfig)
-        , logHook     = projectsLogWithPP (dzenPP leftHandle)
+        , logHook     = projectsLogWithPP (_dzenPP lh)
         , startupHook = initialiseProjects _projectsConfig
         , terminal    = _terminal
         , workspaces  = _staticWorkspaces
